929c741e48eb2cb4fa6a5f8b67c8f5175a602420
jonhumphreys <93622145+jonhumphreys@users.noreply.github.com>
2025-03-20 17:36:33 -0700

    First commit

--- SE320-MethodRewriteInClass/Program.cs
+++ SE320-MethodRewriteInClass/Program.cs
@@ -1,9 +1,32 @@
 ﻿namespace SE320_MethodRewriteInClass;
 
+/*
+ * This program prints out a series of letters in alphabetical order
+ * Usage: PrintResults()
+ * last updated: 4/23/22
+ */
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        PrintResults();
     }
+
+    /*
+     * PrintResults:
+     * this method prints output letters in alphabetical order
+     * Written by: Leonard Smalls
+     * Parameters: none
+     * Returns: none
+     * last updated: 4/23/22
+    */ 
+    public static void PrintResults() 
+    { 
+    	// print the letters in alphabetical order 
+        Console.WriteLine("g"); 
+        Console.WriteLine("j"); 
+        Console.WriteLine("p"); 
+        Console.WriteLine("t"); 
+        Console.WriteLine("w"); 
+    } 
 }
